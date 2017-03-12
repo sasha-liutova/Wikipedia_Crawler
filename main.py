1110de9f4f71e6fdb83cb680dc4647f4ec8f002b
DESKTOP-19IUA25\sasha <sasha.lyutova@gmail.com>
2017-03-12 21:26:29 +0100

    crawling multiple links

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
+
 import scrapy
 
 
-class BlogSpider(scrapy.Spider):
-    name = 'bla'
-    start_urls = ['http://localhost:8000/']
-    custom_settings = {'USER_AGENT':'DDW', 'DOWNLOAD_DELAY':2}
+class QuotesSpider(scrapy.Spider):
+    name = "wiki"
+    start_urls = [
+        'https://en.wikipedia.org/wiki/Color_vision',
+    ]
+    custom_settings = {'USER_AGENT': 'DDW_friendly_bot', 'DOWNLOAD_DELAY': 2,
+                       'DEPTH_LIMIT':5}
 
     def parse(self, response):
-        for title in response.css('h2.entry-title'):
-            yield {'title': title.css('a ::text').extract_first()}
+        # for quote in response.css('div.quote'):
+        #     yield {
+        #         'text': quote.css('span.text::text').extract_first(),
+        #         'author': quote.css('small.author::text').extract_first(),
+        #         'tags': quote.css('div.tags a.tag::text').extract(),
+        #     }
 
-        next_page = response.css('a ::attr(href)').extract_first()
-        if next_page:
-            yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
+        next_pages_raw = response.selector.xpath('//*/a[2]/@href').extract()
+        next_pages = [page for page in next_pages_raw if page.startswith('/wiki/')]
+        for next_page in next_pages:
+            if next_page is not None:
+                print('NEXT PAGE: ', next_page)
+                next_page = response.urljoin(next_page)
+                yield scrapy.Request(next_page, callback=self.parse)
